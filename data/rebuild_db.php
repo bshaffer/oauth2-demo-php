c1db60dfbac802e23e2dc598826aa3901c74bc15
Brent Shaffer <bshafs@gmail.com>
2013-09-06 13:59:46 -0600

    adds user_credentials grant

--- data/rebuild_db.php
+++ data/rebuild_db.php
@@ -22,9 +22,11 @@ $db->exec('CREATE TABLE oauth_clients (client_id TEXT, client_secret TEXT, redir
 $db->exec('CREATE TABLE oauth_access_tokens (access_token TEXT, client_id TEXT, user_id TEXT, expires TIMESTAMP, scope TEXT)');
 $db->exec('CREATE TABLE oauth_authorization_codes (authorization_code TEXT, client_id TEXT, user_id TEXT, redirect_uri TEXT, expires TIMESTAMP, scope TEXT)');
 $db->exec('CREATE TABLE oauth_refresh_tokens (refresh_token TEXT, client_id TEXT, user_id TEXT, expires TIMESTAMP, scope TEXT)');
+$db->exec('CREATE TABLE oauth_users (username TEXT, password TEXT, first_name TEXT, last_name TEXT)');
 
 // add test data
 $db->exec('INSERT INTO oauth_clients (client_id, client_secret) VALUES ("demoapp", "demopass")');
+$db->exec(sprintf('INSERT INTO oauth_users (username, password) VALUES ("demouser", "%s")', sha1("testpass")));
 
 chmod($dbfile, 0777);
 // $db->exec('INSERT INTO oauth_access_tokens (access_token, client_id) VALUES ("testtoken", "Some Client")');
--- src/OAuth2Demo/Client/Controllers/RequestToken.php
+++ src/OAuth2Demo/Client/Controllers/RequestToken.php
@@ -9,10 +9,12 @@ class RequestToken
 {
     static public function addRoutes($routing)
     {
-        $routing->get('/client/request_token', array(new self(), 'requestToken'))->bind('request_token');
+        $routing->get('/client/request_token/authorization_code', array(new self(), 'requestTokenWithAuthCode'))->bind('request_token_with_authcode');
+        $routing->get('/client/request_token/user_credentials', array(new self(), 'requestTokenWithUserCredentials'))->bind('request_token_with_usercredentials');
+
     }
 
-    public function requestToken(Application $app)
+    public function requestTokenWithAuthCode(Application $app)
     {
         $twig   = $app['twig'];          // used to render twig templates
         $config = $app['parameters'];    // the configuration for the current oauth implementation
@@ -28,6 +30,41 @@ class RequestToken
             'client_id'     => $config['client_id'],
             'client_secret' => $config['client_secret'],
             'redirect_uri'  => $urlgen->generate('authorize_redirect', array(), true),
+        );
+
+        // determine the token endpoint to call based on our config (do this somewhere else?)
+        $grantRoute = $config['token_route'];
+        $endpoint = 0 === strpos($grantRoute, 'http') ? $grantRoute : $urlgen->generate($grantRoute, array(), true);
+
+        // make the token request via curl and decode the json response
+        $response = $curl->request($endpoint, $query, 'POST', $config['curl_options']);
+        $json = json_decode($response['response'], true);
+
+        // if it is succesful, display the token in our app
+        if (isset($json['access_token'])) {
+            return $twig->render('client/show_access_token.twig', array('token' => $json['access_token']));
+        }
+
+        return $twig->render('client/failed_token_request.twig', array('response' => $json ? $json : $response));
+    }
+
+    public function requestTokenWithUserCredentials(Application $app)
+    {
+        $twig   = $app['twig'];          // used to render twig templates
+        $config = $app['parameters'];    // the configuration for the current oauth implementation
+        $urlgen = $app['url_generator']; // generates URLs based on our routing
+        $curl   = new Curl();            // simple class used to make curl requests
+
+        $username = $app['request']->get('username');
+        $password = $app['request']->get('password');
+
+        // exchange user credentials for access token
+        $query = array(
+            'grant_type'    => 'password',
+            'client_id'     => $config['client_id'],
+            'client_secret' => $config['client_secret'],
+            'username'      => $username,
+            'password'      => $password,
         );
 
         // determine the token endpoint to call based on our config (do this somewhere else?)
