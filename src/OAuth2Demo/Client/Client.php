ff846d24cdbe615170eab84c1a15dc5dcc5a994f
Brent Shaffer <bshafs@gmail.com>
2013-10-21 05:44:57 -0600

    moves logic from front controller to Client app setup

--- src/OAuth2Demo/Client/Client.php
+++ src/OAuth2Demo/Client/Client.php
@@ -4,24 +4,44 @@ namespace OAuth2Demo\Client;
 
 use Silex\Application;
 use Silex\ControllerProviderInterface;
-use Silex\ControllerCollection;
-use Symfony\Component\HttpFoundation\Response;
+use Silex\Provider\SessionServiceProvider;
 
 class Client implements ControllerProviderInterface
 {
-    public function connect(Application $app)
+    /**
+     * function to set up the container for the Client app
+     */
+    public function setup(Application $app)
     {
-        // creates a new controller based on the default route
-        $routing = $app['controllers_factory'];
+        // create session object and start it
+        $app->register(new SessionServiceProvider());
 
-        /* Set the container */
-        // ensures this runs on default port
+        if (!$app['session']->isStarted()) {
+            $app['session']->start();
+        }
+
+        // create curl object and ensure it runs on default port
         $port = is_numeric($_SERVER['SERVER_PORT']) ? intval($_SERVER['SERVER_PORT']) : 80;
         $app['curl'] = new Http\Curl(array('http_port' => $port));
+
         // add twig extension
         $app['twig']->addExtension(new Twig\JsonStringifyExtension());
 
-        /* Set corresponding endpoints on the controller classes */
+        // load parameters configuration
+        $this->loadParameters($app);
+    }
+
+    /**
+     * Connect the controller classes to the routes
+     */
+    public function connect(Application $app)
+    {
+        // set up the service container
+        $this->setup($app);
+
+        // Load routes from the controller classes
+        $routing = $app['controllers_factory'];
+
         Controllers\Homepage::addRoutes($routing);
         Controllers\ReceiveAuthorizationCode::addRoutes($routing);
         Controllers\RequestToken::addRoutes($routing);
@@ -29,5 +49,28 @@ class Client implements ControllerProviderInterface
         Controllers\ReceiveImplicitToken::addRoutes($routing);
 
         return $routing;
+    }
+
+    /**
+     * Load the parameters configuration
+     */
+    private function loadParameters(Application $app)
+    {
+        $parameterFile = __DIR__.'/../../../data/parameters.json';
+        if (!file_exists($parameterFile)) {
+            // allows you to customize parameter file
+            $parameterFile = $parameterFile.'.dist';
+        }
+        $app['environments'] = array();
+        if (!$parameters = json_decode(file_get_contents($parameterFile), true)) {
+            throw new \Exception('unable to parse parameters file: '.$parameterFile);
+        }
+        // we are using an array of configurations
+        if (!isset($parameters['client_id'])) {
+            $app['environments'] = array_keys($parameters);
+            $env = $app['session']->get('config_environment');
+            $parameters = isset($parameters[$env]) ? $parameters[$env] : array_shift($parameters);
+        }
+        $app['parameters'] = $parameters;
     }
 }
--- web/index.php
+++ web/index.php
@@ -8,36 +8,12 @@ error_reporting(E_ALL);
 
 /** set up the silex application object */
 $app = new Silex\Application();
+$app->register(new Silex\Provider\UrlGeneratorServiceProvider());
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/../views',
 ));
-$app->register(new Silex\Provider\UrlGeneratorServiceProvider());
-$app->register(new Silex\Provider\SessionServiceProvider());
 
 $app['debug'] = true;
-
-/** start the session */
-if (!$app['session']->isStarted()) {
-    $app['session']->start();
-}
-
-/** load the parameters configuration */
-$parameterFile = __DIR__.'/../data/parameters.json';
-if (!file_exists($parameterFile)) {
-    // allows you to customize parameter file
-    $parameterFile = $parameterFile.'.dist';
-}
-$app['environments'] = array();
-if (!$parameters = json_decode(file_get_contents($parameterFile), true)) {
-    throw new Exception('unable to parse parameters file: '.$parameterFile);
-}
-// we are using an array of configurations
-if (!isset($parameters['client_id'])) {
-    $app['environments'] = array_keys($parameters);
-    $env = $app['session']->get('config_environment');
-    $parameters = isset($parameters[$env]) ? $parameters[$env] : array_shift($parameters);
-}
-$app['parameters'] = $parameters;
 
 /** set up routes / controllers */
 $app->mount('/', new OAuth2Demo\Client\Client());
